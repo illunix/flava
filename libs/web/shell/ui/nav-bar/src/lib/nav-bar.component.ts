9b574a5a1a921e4e78b0e5e7b02f1ca7e0e9d5ee
Maks Szokalski <cros448@gmail.com>
2022-06-06 23:47:19 +0200

    update

--- libs/web/shell/ui/nav-bar/src/lib/nav-bar.component.ts
+++ libs/web/shell/ui/nav-bar/src/lib/nav-bar.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { SignInDialogComponent } from '@flava/web/shell/ui/sign-in-dialog';
 
 @Component({
   selector: 'flava-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  constructor(private dialog: MatDialog) {}
+
+  public onToggleSignInDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    // dialogConfig.panelClass = "my-mat-dialog-container";
+    
+    this.dialog.open(
+      SignInDialogComponent, 
+      dialogConfig
+    )
+      .afterClosed()
+      .subscribe();
+  }
+}
--- libs/web/shell/ui/sign-in-dialog/src/index.ts
+++ libs/web/shell/ui/sign-in-dialog/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/sign-in-dialog.module';
+export * from './lib/sign-in-dialog.component';
